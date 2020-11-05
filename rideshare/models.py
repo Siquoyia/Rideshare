de8375e5fb725a5bc2bbfa03f0670e50392ab9af
Siquoyia <70215872+Siquoyia@users.noreply.github.com>
2020-11-05 13:20:12 -0800

    rideshare update

--- rideshare/models.py
+++ rideshare/models.py
@@ -1,3 +1,35 @@
+from datetime import datetime
 from django.db import models
 
+
+PASSENGER_CHOICES = (
+  (1,1),
+  (2,2),
+  (3,3),
+)  
+
+
+
+
 # Create your models here.
+class RideShare(models.Model):
+
+    passenger = models.ForeignKey("Passenger",on_delete=models.CASCADE,related_name="rideshares")
+    driver = models.ForeignKey(max_length=255,null=True, blank=True)
+    schedule = models.DateTimeField(default=datetime.now)
+    is_complete = models.BooleanField(default=False)
+    number_of_passengers = models.ChoiceField(choices=PASSENGER_CHOICES)
+    destination = models.ForeignKey("Destination",on_delete=models.CASCADE,related_name="rideshares")
+
+class Destination(models.Model):
+    name = models.CharField(max_length=255,null=True, blank=True)
+    address = models.TextField(max_length=255,null=True, blank=True)
+    link = models.UrlField()    
+    
+class Passenger(models.Model):
+    name = models.CharField(max_length=255,null=True, blank=True)
+    address = models.TextField(max_length=255,null=True, blank=True)
+
+class Driver(models.Model):
+    name = models.CharField(max_length=255,null=True, blank=True)
+    vehicle_description = models.CharField(max_length=255,null=True, blank=True)
